e58bc45e8a43f4a8c02d539628342a37c811ddc5
tihuep <timon.hueppi@gmail.com>
2022-01-12 23:37:20 +0100

    adds basic layout of color control

--- app/src/main/java/ch/timonhueppi/etg/jugilightcontroller/MainActivity.java
+++ app/src/main/java/ch/timonhueppi/etg/jugilightcontroller/MainActivity.java
@@ -3,12 +3,41 @@ package ch.timonhueppi.etg.jugilightcontroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar number1;
+    EditText number2;
+    EditText number3;
+    EditText number4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        number1 = findViewById(R.id.number1);
+        number2 = findViewById(R.id.number2);
+        number3 = findViewById(R.id.number3);
+        number4 = findViewById(R.id.number4);
+
+        number1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                System.out.println("onprogresschanged");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                System.out.println("onstarttrackingtouch");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                System.out.println("onstoptrackingtouch");
+            }
+        });
     }
 }
